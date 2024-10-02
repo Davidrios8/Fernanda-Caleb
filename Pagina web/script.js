2f3b678465b727f436d206758fd5a745c09983dd
Davidrios8 <riosjuarezcaleb97@gmail.com>
2024-10-01 22:16:37 -0600

    Add files via upload

---
+++ Pagina web/script.js
@@ -0,0 +1,21 @@
+// Set the target date
+var countDownDate = new Date("Nov 10, 2024 23:59:59").getTime();
+
+// Update the countdown every 1 second
+var countdownFunction = setInterval(function() {
+    var now = new Date().getTime();
+    var distance = countDownDate - now;
+
+    var days = Math.floor(distance / (1000 * 60 * 60 * 24));
+    var hours = Math.floor((distance % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
+    var minutes = Math.floor((distance % (1000 * 60 * 60)) / (1000 * 60));
+    var seconds = Math.floor((distance % (1000 * 60)) / 1000);
+
+    document.getElementById("countdown").innerHTML = days + "d " + hours + "h "
+    + minutes + "m " + seconds + "s ";
+
+    if (distance < 0) {
+        clearInterval(countdownFunction);
+        document.getElementById("countdown").innerHTML = "EXPIRED";
+    }
+}, 1000);
